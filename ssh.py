45ce445a991f5e460244c9c095c4c8751d76e22c
Drew Fink <me@drewf.ink>
2023-11-28 20:14:07 -0600

    Start on SSH honeypot (& create a ridiculous mess)

---
+++ ssh.py
@@ -0,0 +1,99 @@
+import csv
+from Crypto.PublicKey import RSA
+import datetime
+import os
+import requests
+import socket
+import sys
+import threading
+import _thread
+import paramiko
+import yaml
+
+# Load yaml config
+config = yaml.safe_load(open("config.yml"))
+
+# Generate keys if they don't exist
+if not os.path.exists("server.key"):
+    key = RSA.generate(3072)
+    with open("server.key", 'wb') as content_file:
+        content_file.write(key.exportKey('PEM'))
+if not os.path.exists("server.pub"):
+    pubkey = key.publickey()
+    with open("server.pub", 'wb') as content_file:
+        content_file.write(pubkey.exportKey('OpenSSH'))
+server_key = paramiko.RSAKey(filename='server.key')
+
+port = 6222
+
+# CSV log name
+csv_outfile = "ssh_attempts.csv"
+
+class Honeypot(paramiko.ServerInterface):
+    def __init__(self, connection):
+        self.event = threading.Event()
+        self.connection = connection
+        self.ip_country = "Unknown"
+        self.ip_region = "Unknown"
+        self.ip_city = "Unknown"
+        self.ip_isp = "Unknown"
+
+        # Gather location data using IP-API
+        url = f"http://ip-api.com/json/{connection}"
+        resp = requests.get(url=url).json()
+        if(resp['status'] == 'success'):
+            self.ip_country = resp['country']
+            self.ip_region = resp['regionName']
+            self.ip_city = resp['city']
+            self.ip_isp = resp['isp']
+
+    def check_auth_password(self, username, password):
+        current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        if config['csv_logging']:
+            with open(csv_outfile, 'a', newline='') as attempt:
+                outfile = csv.writer(attempt, quoting=csv.QUOTE_MINIMAL)
+                outfile.writerow([current_time, username, password, self.connection,
+                                  self.ip_country, self.ip_region, self.ip_city, self.ip_isp])
+        return paramiko.AUTH_FAILED
+
+    def get_allowed_auths(self, username):
+        return 'password'
+
+def handleConnection(client):
+    transport = paramiko.Transport(client)
+    transport.add_server_key(server_key)
+
+    ip = transport.getpeername()[0]
+
+    transport.start_server(server=Honeypot(ip))
+
+    channel = transport.accept(1)
+    if not channel is None:
+        channel.close()
+
+if __name__ == '__main__':
+    # Create a new CSV log file if it's enabled and doesn't exist already
+    if config['csv_logging'] and not os.path.exists(csv_outfile):
+        with open(csv_outfile, 'w', newline='') as atts:
+            initial = csv.writer(atts, quoting=csv.QUOTE_MINIMAL)
+            initial.writerow(["Time", "Username", "Password", "IP", "Country", "Region", "City", "ISP"])
+    try:
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_socket.bind(('', port))
+        server_socket.listen(100)
+        while(True):
+            try:
+                client_socket, client_addr = server_socket.accept()
+                _thread.start_new_thread(handleConnection,(client_socket,))
+            except KeyboardInterrupt:
+                end_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                print(f"[{end_time}] Quitting.")
+                try:
+                    sys.exit(130)
+                except SystemExit:
+                    os._exit(130)
+    except Exception as e:
+        print("Error: Couldn't create socket.")
+        print(e)
+        sys.exit(1)
